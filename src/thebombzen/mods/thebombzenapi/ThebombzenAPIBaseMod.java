b20e57c4cbacc1179908e05ad97aee378c316870
Leo Izen <leo.izen@gmail.com>
2015-12-29 12:53:16 -0500

    Attempted fix at "Overworld not loaded" bug

--- src/thebombzen/mods/thebombzenapi/ThebombzenAPIBaseMod.java
+++ src/thebombzen/mods/thebombzenapi/ThebombzenAPIBaseMod.java
@@ -17,6 +17,7 @@ import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.storage.SaveHandler;
+import net.minecraftforge.common.DimensionManager;
 import thebombzen.mods.thebombzenapi.configuration.ThebombzenAPIConfiguration;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -80,7 +81,27 @@ public abstract class ThebombzenAPIBaseMod implements Comparable<ThebombzenAPIBa
 	 * Debug output goes to this file.
 	 */
 	protected File debugFile;
-
+	
+	/**
+	 * If this flag is set to true, then we need to reload the memory from the correct memory file.
+	 */
+	protected boolean dirtyMemoryFlag = true;
+	
+	/**
+	 * Mark the memory for reloading from the memory file. This is useful if the world has changed or just loaded.
+	 */
+	public void markMemoryDirty(){
+		dirtyMemoryFlag = true;
+	}
+	
+	/**
+	 * Returns true if the memory should be reloaded from the memory file
+	 * and false if the memory does not need to be reloaded from the memory file.
+	 */
+	public boolean isMemoryDirty(){
+		return dirtyMemoryFlag;
+	}
+	
 	@Override
 	public int compareTo(ThebombzenAPIBaseMod mod){
 		if (this == mod){
@@ -211,13 +232,13 @@ public abstract class ThebombzenAPIBaseMod implements Comparable<ThebombzenAPIBa
 			return null;
 		}
 		if (Minecraft.getMinecraft().isSingleplayer()) {
-			return new File(((SaveHandler) Minecraft
-					.getMinecraft()
-					.getIntegratedServer()
-					.worldServerForDimension(
-							Minecraft.getMinecraft().thePlayer.dimension)
-					.getSaveHandler()).getWorldDirectory(), getLongName()
-					.toUpperCase() + "_MEMORY.dat");
+			if (ThebombzenAPI.hasServerWorldLoaded()){
+				return new File(((SaveHandler) DimensionManager.getWorld(Minecraft.getMinecraft().thePlayer.dimension)
+						.getSaveHandler()).getWorldDirectory(),
+						getLongName().toUpperCase() + "_MEMORY.dat");
+			} else {
+				return null;
+			}
 		} else {
 			return new File(getModFolder(),
 					getLongName().toUpperCase()
@@ -401,9 +422,9 @@ public abstract class ThebombzenAPIBaseMod implements Comparable<ThebombzenAPIBa
 		return toggles[index];
 	}
 	
-	
+
 	/**
-	 * Load memory data from the correct memory file.
+	 * Load the saved data (currently only toggles) from the correct memory file.
 	 */
 	@SideOnly(Side.CLIENT)
 	public void readFromCorrectMemoryFile() {
@@ -411,6 +432,7 @@ public abstract class ThebombzenAPIBaseMod implements Comparable<ThebombzenAPIBa
 		if (file != null) {
 			NBTTagCompound data = readFromMemoryFile(file);
 			saveCompoundToCurrentData(data);
+			dirtyMemoryFlag = false;
 		}
 	}
 	
